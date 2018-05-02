bf749c39a3fdc5b17857c89eb343d52301efb9e4
zerhaz <m.stancelewski@gmail.com>
2018-05-02 23:46:26 +0200

    new class
    application.java

---
+++ src/zerhaz/pl/Application.java
@@ -0,0 +1,7 @@
+package zerhaz.pl;
+
+public class Application {
+    public static void popUp(){
+        System.out.println("Bye, Bye!");
+    }
+}
--- src/zerhaz/pl/Main.java
+++ src/zerhaz/pl/Main.java
@@ -4,10 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Hello World");
-
-	    for(int i = 0; i < 11; i++){
-	        System.out.println(i);
-        }
-        System.out.println("End Of File");
+        Application.popUp();
     }
 }
