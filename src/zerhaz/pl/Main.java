ec3d9e80727c2c5d28dc056709a8af0bc3eebe77
zerhaz <m.stancelewski@gmail.com>
2018-05-02 17:57:50 +0200

    change 1

--- src/zerhaz/pl/Main.java
+++ src/zerhaz/pl/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Hello World");
+
+	    for(int i = 0; i < 11; i++){
+	        System.out.println(i);
+        }
     }
 }
