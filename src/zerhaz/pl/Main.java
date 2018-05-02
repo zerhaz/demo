eed32034cb5fa098437745d9f9ed890f4773a73c
zerhaz <m.stancelewski@gmail.com>
2018-05-02 18:33:28 +0200

    change 4

--- src/zerhaz/pl/Main.java
+++ src/zerhaz/pl/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("Hello World");
 
-	    for(int i = 0; i < 30; i++){
+	    for(int i = 0; i < 11; i++){
 	        System.out.println(i);
         }
         System.out.println("End Of File");
